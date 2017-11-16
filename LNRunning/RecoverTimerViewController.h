f05a915fdb11c4725363f77c531f327540c69901
Lix <xiao.liunit@gmail.com>
2017-11-16 10:19:00 +0800

    recover timer controller

---
+++ LNRunning/RecoverTimerViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecoverTimerViewController.h
+//  LNRunning
+//
+//  Created by Lix on 2017/11/16.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecoverTimerViewController : UIViewController
+
+@end
---
+++ LNRunning/RecoverTimerViewController.m
@@ -0,0 +1,44 @@
+//
+//  RecoverTimerViewController.m
+//  LNRunning
+//
+//  Created by Lix on 2017/11/16.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import "RecoverTimerViewController.h"
+#import "LNRunning-Swift.h"
+
+@interface RecoverTimerViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property (weak, nonatomic) IBOutlet UIButton *startBtn;
+@property (weak, nonatomic) IBOutlet UIButton *pauseBtn;
+@property (weak, nonatomic) IBOutlet UIButton *resetBtn;
+@property (nonatomic, strong) LNRecoverTimer *timer;
+@end
+
+@implementation RecoverTimerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.timer = [[LNRecoverTimer alloc] initWithTimeLabel:self.timeLabel];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (IBAction)start:(id)sender {
+    [self.timer timingStart];
+}
+
+- (IBAction)pause:(id)sender {
+    [self.timer timingPause];
+}
+
+- (IBAction)reset:(id)sender {
+    [self.timer resetToStart];
+}
+
+@end
