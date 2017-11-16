acf2deebcca6acf37e78cb71bfb11426ac1a0f9b
Lix <xiao.liunit@gmail.com>
2017-11-16 11:21:37 +0800

    create run table

--- LNRunning/Classes/Model/LNRunPointModel.h
+++ LNRunning/Classes/Model/LNRunPointModel.h
@@ -10,4 +10,13 @@
 
 @interface LNRunPointModel : NSObject
 
+@property (nonatomic, assign) NSInteger runId;
+@property (nonatomic, copy) NSString *time;
+@property (nonatomic, assign) double latitude;
+@property (nonatomic, assign) double longitude;
+@property (nonatomic, assign) NSInteger accuration;
+@property (nonatomic, assign) NSInteger distance;
+@property (nonatomic, assign) NSInteger duration;
+@property (nonatomic, assign) NSInteger steps;
+
 @end
