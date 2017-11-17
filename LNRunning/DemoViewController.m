da72dc2409432b7f1f3a53cf1fe96852f771f426
Lix <xiao.liunit@gmail.com>
2017-11-17 14:57:05 +0800

    demo 约束调整完毕

--- LNRunning/DemoViewController.m
+++ LNRunning/DemoViewController.m
@@ -17,13 +17,20 @@
 @property (weak, nonatomic) IBOutlet UILabel *frequencyLabel;
 @property (weak, nonatomic) IBOutlet UILabel *strideLabel;
 @property (weak, nonatomic) IBOutlet UILabel *timeLabel;
-
+@property (nonatomic, weak) IBOutlet UIButton *pause1Button;
+@property (nonatomic, weak) IBOutlet UIButton *continue1Button;
+@property (nonatomic, weak) IBOutlet UIButton *end1Button;
 @end
 
 @implementation DemoViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+//    [self ln_updateConstraints];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
     [self ln_updateConstraints];
 }
 
@@ -37,6 +44,28 @@
     [self.lnMapView setFrame:Rect(self.mainView.x, self.mainView.y, self.mainView.width, self.mainView.height)];
     [self.timerLabel setFrame:Rect(self.timeLabel.x, self.timeLabel.y, self.timeLabel.width, self.timeLabel.height)];
     [self.timeLabel setHidden:YES];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self.timerLabel setFrame:self.timeLabel.frame];
+        self.timerLabel.textColor = self.timeLabel.textColor;
+//        self.pause1Button = self.pauseButton;
+//        self.continue1Button = self.continueButton;
+//        self.end1Button = self.endButton;
+//        [self.pause1Button setHidden:true];
+//        [self.continue1Button setHidden:true];
+//        [self.end1Button setHidden:true];
+        
+        [self.pauseButton setFrame:self.pause1Button.frame];
+        [self.continueButton setFrame:self.continue1Button.frame];
+        [self.endButton setFrame:self.end1Button.frame];
+    });
+//    [self.timerLabel setFrame:self.timeLabel.frame];
+//    self.timerLabel.textColor = self.timeLabel.textColor;
+//    self.pause1Button = self.pauseButton;
+//    self.continue1Button = self.continueButton;
+//    self.end1Button = self.endButton;
+//    [self.pauseButton setHidden:true];
+//    [self.continueButton setHidden:true];
+//    [self.endButton setHidden:true];
 }
 
 - (void)ln_bindRunData {
