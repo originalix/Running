800ca77942885d682887e1259b929fd68cedd32e
Lix <xiao.liunit@gmail.com>
2017-11-20 11:21:36 +0800

    create Recover Manager

---
+++ LNRunning/Classes/Model/RecoverManager.h
@@ -0,0 +1,16 @@
+//
+//  RecoverManager.h
+//  LNRunning
+//
+//  Created by Lix on 2017/11/20.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/*
+ * 处理非正常结束跑步之后的恢复逻辑类
+ */
+@interface RecoverManager : NSObject
+
+@end
---
+++ LNRunning/Classes/Model/RecoverManager.m
@@ -0,0 +1,13 @@
+//
+//  RecoverManager.m
+//  LNRunning
+//
+//  Created by Lix on 2017/11/20.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import "RecoverManager.h"
+
+@implementation RecoverManager
+
+@end
